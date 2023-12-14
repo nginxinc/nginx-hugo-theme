f929de954a6926f0a2f086aab8a56c834e149ec8
Jon Cahill-Torre <j.cahilltorre@f5.com>
2023-12-14 15:53:38 +0000

    fix: order for list pages + navbar improv

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -4,12 +4,31 @@ $(document).ready(function () {
         if (item.dataset.menuId == $(".main").data("menuId")) {
             $(item).css("color", "#429345");
             $(item).css("font-weight", "500");
+
+            // Remove "collapsed" class and set aria-expanded to "true" for the current item
+            $(item).removeClass("collapsed");
+            $(item).attr("aria-expanded", "true");
+            
+            // Capture data-target value
+            var targetId = $(item).data("target");
+            if (!targetId) {
+                var hrefValue = $(item).attr("href");
+                // Add "show" class to the corresponding divs with matching id
+                var targetDivs = $("div[id='" + hrefValue.replace("#", "") + "']");
+                targetDivs.addClass("show");
+            } else {
+                $(targetId).addClass("show");
+            }
+            
+            // Expand parents
             $(item).parents(".collapse").each(function(i,el) {
                 var col = new bootstrap.Collapse(el, {
                     toggle: false
                 });
                 col.show();
             });
-      }
-      });
+
+            $(item).next(".accordion-body").find(".collapse").addClass("show");
+        }
+        });
 });

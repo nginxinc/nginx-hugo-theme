16052517a80843e07b07adcfc8e513a77314fac8
Luke Billington <lukecreative@me.com>
2024-02-12 09:34:27 -0800

    Add toggle-able sidebar button (#47)
    * feat: Initial sidebar modification to support expand/collapse
    
    * feat: add resize
    
    * feat: add default resize
    
    * fix: fix widths of content
    
    * fix: Fix redoc widths
    
    * feat: Add aria attributes
    
    * fix: fixed issue with content width on safari
    
    * fix: added fix to weird seam when scrollbar always is set in mac settings
    
    ---------
    
    Co-authored-by: Luke Billington <l.billington@f5.com>
    Co-authored-by: Lam Nguyen <la.nguyen@f5.com>

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,34 +1,92 @@
+const LOCAL_STORAGE_COLLAPSE_KEY = "sidebar-collapsed";
+
+function handleInitialCollapse() {
+  const collapsed = localStorage.getItem(LOCAL_STORAGE_COLLAPSE_KEY);
+
+  if (collapsed === "true") {
+    $("#sidebar-toggle-button").attr({
+      "aria-expanded": "false",
+      title: "Show sidebar navigation",
+    });
+    $("#sidebar-wrapper").addClass("sidebar-toggle-collapsed");
+    $(".sidenav").addClass("sidebar-col-collapsed-width");
+    $(".content").addClass("sidebar-content-collapsed-width");
+    $(".nginx-docs-api-container").addClass("sidebar-redoc-collapsed-width");
+  } else {
+    $("#sidebar-toggle-button").attr({
+      "aria-expanded": "true",
+      title: "Hide sidebar navigation",
+    });
+    $("#sidebar-wrapper").addClass("hide-sidebar-border");
+  }
+}
+
+function handleSetCollapse() {
+  $("#sidebar-wrapper").addClass("sidebar-toggle-animate");
+
+  const collapsed = localStorage.getItem(LOCAL_STORAGE_COLLAPSE_KEY);
+
+  if (collapsed === "false") {
+    $("#sidebar-toggle-button").attr({
+      "aria-expanded": "false",
+      title: "Show sidebar navigation",
+    });
+    $("#sidebar-wrapper").addClass("sidebar-toggle-collapsed");
+    $(".sidenav").addClass("sidebar-col-collapsed-width");
+    $(".content").addClass("sidebar-content-collapsed-width");
+    $(".nginx-docs-api-container").addClass("sidebar-redoc-collapsed-width");
+    localStorage.setItem(LOCAL_STORAGE_COLLAPSE_KEY, true);
+  } else {
+    $("#sidebar-toggle-button").attr({
+      "aria-expanded": "true",
+      title: "Hide sidebar navigation",
+    });
+    $("#sidebar-wrapper").addClass("hide-sidebar-border");
+    $("#sidebar-wrapper").removeClass("sidebar-toggle-collapsed");
+    $(".sidenav").delay(500).removeClass("sidebar-col-collapsed-width");
+    $(".content").removeClass("sidebar-content-collapsed-width");
+    $(".nginx-docs-api-container").removeClass("sidebar-redoc-collapsed-width");
+    localStorage.setItem(LOCAL_STORAGE_COLLAPSE_KEY, false);
+  }
+}
+
 // This code makes the sidebar remember which sections has been clicked when using the sidebar
 $(document).ready(function () {
-    $(".sidebar .nginx-toc-link a").each(function(i,item) {
-        if (item.dataset.menuId == $(".main").data("menuId")) {
-            $(item).css("color", "#429345");
-            $(item).css("font-weight", "500");
+  // sidebar toggle navigation logic
+  handleInitialCollapse();
+  $("#sidebar-toggle-button").on("click", handleSetCollapse);
 
-            // Remove "collapsed" class and set aria-expanded to "true" for the current item
-            $(item).removeClass("collapsed");
-            $(item).attr("aria-expanded", "true");
-            
-            // Capture data-target value
-            var targetId = $(item).data("target");
-            if (!targetId) {
-                var hrefValue = $(item).attr("href");
-                // Add "show" class to the corresponding divs with matching id
-                var targetDivs = $("div[id='" + hrefValue.replace("#", "") + "']");
-                targetDivs.addClass("show");
-            } else {
-                $(targetId).addClass("show");
-            }
-            
-            // Expand parents
-            $(item).parents(".collapse").each(function(i,el) {
-                var col = new bootstrap.Collapse(el, {
-                    toggle: false
-                });
-                col.show();
-            });
+  $(".sidebar .nginx-toc-link a").each(function (i, item) {
+    if (item.dataset.menuId == $(".main").data("menuId")) {
+      $(item).css("color", "#429345");
+      $(item).css("font-weight", "500");
 
-            $(item).next(".accordion-body").find(".collapse").addClass("show");
-        }
+      // Remove "collapsed" class and set aria-expanded to "true" for the current item
+      $(item).removeClass("collapsed");
+      $(item).attr("aria-expanded", "true");
+
+      // Capture data-target value
+      var targetId = $(item).data("target");
+      if (!targetId) {
+        var hrefValue = $(item).attr("href");
+        // Add "show" class to the corresponding divs with matching id
+        var targetDivs = $("div[id='" + hrefValue.replace("#", "") + "']");
+        targetDivs.addClass("show");
+      } else {
+        $(targetId).addClass("show");
+      }
+
+      // Expand parents
+      $(item)
+        .parents(".collapse")
+        .each(function (i, el) {
+          var col = new bootstrap.Collapse(el, {
+            toggle: false,
+          });
+          col.show();
         });
+
+      $(item).next(".accordion-body").find(".collapse").addClass("show");
+    }
+  });
 });

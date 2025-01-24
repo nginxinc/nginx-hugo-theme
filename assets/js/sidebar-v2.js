9353c1102e98de805f59faf915b480cc8a846e68
Lam Nguyen <la.nguyen@f5.com>
2025-01-24 06:50:46 -0800

    small tweaks

--- assets/js/sidebar-v2.js
+++ assets/js/sidebar-v2.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     function expandToCurrentPage() {
-        var currentPage = document.getElementById("current-page");
+        const currentPage = document.getElementById("current-page");
         if (currentPage) {
-            var parentLabel = currentPage.closest("li");
+            let parentLabel = currentPage.closest("li");
             while (parentLabel) {
                 var checkbox = parentLabel.querySelector(".toggle-checkbox");
                 if (checkbox) {

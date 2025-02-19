a34fcfaa9b6df438fef2943d70b1e3366f2c4b2a
Lam Nguyen <la.nguyen@f5.com>
2025-02-19 13:48:06 -0800

    chore: linted js + css files with biome

--- assets/js/code-copy.js
+++ assets/js/code-copy.js
@@ -1,16 +1,16 @@
 function CopyCode(clipboard) {
-  document.querySelectorAll(".highlight").forEach((codeBlock) => {
-    const button = document.createElement("button");
-    button.className = "code-copy";
-    button.type = "button";
+  document.querySelectorAll('.highlight').forEach((codeBlock) => {
+    const button = document.createElement('button');
+    button.className = 'code-copy';
+    button.type = 'button';
     button.innerHTML = '<i class="fas fa-copy"></i> Copy';
 
-    button.addEventListener("click", async () => {
+    button.addEventListener('click', async () => {
       try {
         await clipboard.writeText(
           codeBlock.textContent
-            .replace(/^\s*\d+\s/gm, "") // remove line numbers
-            .replace(/^\s*|\s*$/g, "") // remove carriage returns at top and bottom of block
+            .replace(/^\s*\d+\s/gm, '') // remove line numbers
+            .replace(/^\s*|\s*$/g, '') // remove carriage returns at top and bottom of block
         );
 
         button.blur(); /* Chrome fix */
--- assets/js/site-dropdown.js
+++ assets/js/site-dropdown.js
@@ -1,27 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownContent = document.getElementById("dropdown-content");
-    const navbarButton = document.getElementById("navbar-sites-button");
-    const chevronIcon = document.getElementById("navbar-sites-button-icon");
+document.addEventListener('DOMContentLoaded', () => {
+  const dropdownContent = document.getElementById('dropdown-content');
+  const navbarButton = document.getElementById('navbar-sites-button');
+  const chevronIcon = document.getElementById('navbar-sites-button-icon');
 
-    navbarButton.addEventListener("click", function () {
+  navbarButton.addEventListener('click', () => {
+    chevronIcon.classList.toggle('rotate-chevron');
+
+    if (dropdownContent.style.display === 'block') {
+      dropdownContent.style.display = 'none';
+      navbarButton.classList.remove('remove-bottom-radius');
+    } else {
+      dropdownContent.style.display = 'block';
+      navbarButton.classList.add('remove-bottom-radius');
+    }
+  });
+
+  window.addEventListener('click', (event) => {
+    if (
+      !event.target.matches('#navbar-sites-button') &&
+      !event.target.matches('#navbar-sites-button-icon')
+    ) {
+      if (
+        dropdownContent.style.display !== 'none' &&
+        dropdownContent.style.display !== ''
+      ) {
         chevronIcon.classList.toggle('rotate-chevron');
-
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-            navbarButton.classList.remove("remove-bottom-radius");
-        } else {
-            dropdownContent.style.display = "block";
-            navbarButton.classList.add("remove-bottom-radius");
-        }
-    });
-
-    window.addEventListener("click", function (event) {
-        if (!event.target.matches('#navbar-sites-button') && !event.target.matches('#navbar-sites-button-icon')) {
-            if (dropdownContent.style.display !== "none" && dropdownContent.style.display !== "") {
-                chevronIcon.classList.toggle('rotate-chevron');
-            }
-            dropdownContent.style.display = "none";
-            navbarButton.classList.remove("remove-bottom-radius");
-        }
-    });
+      }
+      dropdownContent.style.display = 'none';
+      navbarButton.classList.remove('remove-bottom-radius');
+    }
+  });
 });

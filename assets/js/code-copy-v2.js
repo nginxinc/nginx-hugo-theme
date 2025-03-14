93d5d77dd9219cdb84d4ae88462cbad83bf7a2d3
Jack Hickey <133868041+nginx-jack@users.noreply.github.com>
2025-03-14 13:46:59 +0000

    Codeblock: Add code type and copy button
    Use render codeblock hook to add head for code type and copy button
    Add code type pulled from render codeblock hook
    Add code-copy-v2 which has click handler for new copy button

---
+++ assets/js/code-copy-v2.js
@@ -0,0 +1,38 @@
+// Tightly coupled to `render-codeblock.html` for element targeting
+const copyToClipBoard = ((clipboard) => async (button) => {
+  const codeBlock = button.parentNode.nextElementSibling;
+
+  if (!codeBlock) {
+    console.error('No code block found to copy from.');
+    button.innerHTML = 'Error';
+    return;
+  }
+
+  const cleanCode = codeBlock.textContent
+    .replace(/^\s*\d+\s/gm, '') // Remove line numbers
+    .trim();
+
+  try {
+    await clipboard.writeText(cleanCode);
+
+    updateButtonState(button, 'Copied!', 2000, true);
+  } catch (error) {
+    updateButtonState(button, 'Error');
+    console.error(error);
+  }
+})(navigator.clipboard);
+
+const updateButtonState = (button, message, revertDelay, disable = false) => {
+  button.innerHTML = message;
+
+  if (disable) {
+    button.disabled = true;
+  }
+
+  if (revertDelay) {
+    setTimeout(() => {
+      button.innerHTML = 'Copy';
+      button.disabled = false;
+    }, revertDelay);
+  }
+};

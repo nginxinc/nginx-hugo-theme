4e4d72e8e19ea7f5b1ed5628d9cdb992584adc41
Lam Nguyen <la.nguyen@f5.com>
2025-03-27 07:47:51 -0700

    fix: Product selector click is not working fully

--- assets/js/product-selector.js
+++ assets/js/product-selector.js
@@ -4,11 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     'product-selector-button'
   );
 
-  if (productSelectorButton === null || productSelectorButton == null) {
+  if (productSelectorButton === null || productSelectorContent == null) {
     return;
   }
 
   productSelectorButton.addEventListener('click', () => {
+    /* Logic for hiding/showing ONLY when the button is clicked */
     if (productSelectorContent.style.display === 'block') {
       productSelectorContent.style.display = 'none';
       productSelectorButton.classList.remove('remove-bottom-radius');
@@ -19,12 +20,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   window.addEventListener('click', (event) => {
-    if (
-      !event.target.matches('#product-selector-button') &&
-      !event.target.matches('#product-selector-button-icon')
-    ) {
+    /* Greedy Logic to hide the product selector when something other than the button is clicked. Assumes everything has an id containing "product-selector" */
+    if (!event.target.id.includes('product-selector')) {
       productSelectorContent.style.display = 'none';
+      productSelectorButton.classList.remove('remove-bottom-radius');
     }
-    productSelectorButton.classList.remove('remove-bottom-radius');
   });
 });

d6d80cac2553fbf18e32959b8f91812e4137990e
Lam Nguyen <la.nguyen@f5.com>
2025-06-09 07:49:17 -0700

    Mainframe: Disabled the visual regression test

--- tests/src/visual-regression.spec.js
+++ tests/src/visual-regression.spec.js
@@ -1,18 +1,18 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('Testing old theme', () => {
-  test('should ensure the old theme does not visually regress', async ({
-    page,
-  }) => {
-    const currentPlus = 'nginx/installing-nginx-open-source/';
-    await page.goto(`/${currentPlus}`);
-    await expect(
-      await page.locator('data-testid=base-layout').count()
-    ).toBeTruthy();
+// test.describe('Testing old theme', () => {
+//   test('should ensure the old theme does not visually regress', async ({
+//     page,
+//   }) => {
+//     const currentPlus = 'nginx/installing-nginx-open-source/';
+//     await page.goto(`/${currentPlus}`);
+//     await expect(
+//       await page.locator('data-testid=base-layout').count()
+//     ).toBeTruthy();
 
-    await page.waitForFunction(() => window.scrollY === 0);
-    await expect(page).toHaveScreenshot('example-site-screenshot.png', {
-      fullPage: true,
-    });
-  });
-});
+//     await page.waitForFunction(() => window.scrollY === 0);
+//     await expect(page).toHaveScreenshot('example-site-screenshot.png', {
+//       fullPage: true,
+//     });
+//   });
+// });

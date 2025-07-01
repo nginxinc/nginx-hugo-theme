d69b9976b31ac5dcd2850ff45e7e7250de154028
Lam Nguyen <la.nguyen@f5.com>
2025-07-01 16:17:48 -0700

    Tests: Added new UI smoke tests

---
+++ tests/src/product-landing.spec.js
@@ -0,0 +1,13 @@
+import { test } from '@playwright/test';
+import { runSmokeTestOnPage } from './util';
+
+test.describe('Smoke test for landing page', () => {
+  test('should test product landing page renders', async ({ page }) => {
+    const products = ['test-product', 'nginx'];
+
+    for (const product of products) {
+      await page.goto(`/${product}/`);
+      await runSmokeTestOnPage(page);
+    }
+  });
+});
---
+++ tests/src/util.js
@@ -0,0 +1,12 @@
+import { expect } from '@playwright/test';
+
+export async function runSmokeTestOnPage(page) {
+  /* Ensure each page follows the following dom structure */
+  await expect(await page.locator('data-testid=content').count()).toBeTruthy();
+  await expect(await page.locator('data-testid=header').count()).toBeTruthy();
+  await expect(await page.locator('data-testid=sidebar').count()).toBeTruthy();
+  await expect(await page.locator('data-testid=footer').count()).toBeTruthy();
+  await expect(
+    await page.locator('data-testid=not-found-container').count()
+  ).toBe(0);
+}

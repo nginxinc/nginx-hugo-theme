daa7ea584da25400b0f18e9e4e735f6d9eadad74
Lam Nguyen <la.nguyen@f5.com>
2025-07-03 10:44:18 -0700

    Tests: Fixed linter issues

--- tests/src/footer.spec.js
+++ tests/src/footer.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('Smoke test for footer', () => {
   test.beforeEach(async ({ page }) => {
--- tests/src/header.spec.js
+++ tests/src/header.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('Smoke test for header', () => {
   test.beforeEach(async ({ page }) => {

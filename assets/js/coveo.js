230ddf22ddec11cb189f2edeed5d91564d07188b
Lam <150060045+lamATnginx@users.noreply.github.com>
2025-10-17 08:49:20 -0700

    Coveo: Fixed reloading causing query to clear + tweaked styling (#427)

--- assets/js/coveo.js
+++ assets/js/coveo.js
@@ -74,13 +74,7 @@ async function atomicCoveo() {
   await customElements.whenDefined('atomic-search-interface');
   const credentials = await getValidSearchCredentials();
 
-  const interfaces = [
-    document.querySelector('#search-v2'),
-    document.querySelector('#search-standalone-header'),
-    document.querySelector('#search-standalone-sidebar'),
-  ].filter(Boolean);
-
-  for (const el of interfaces) {
+  document.querySelectorAll('atomic-search-interface').forEach(async (el) => {
     await el.initialize({
       ...credentials,
       analytics: { analyticsMode: 'legacy' },
@@ -92,7 +86,7 @@ async function atomicCoveo() {
       },
     });
     await el.executeFirstSearch();
-  }
+  });
 
   const headerSearchBar = document.querySelector('#search-standalone-header');
   if (headerSearchBar?.shadowRoot) {
